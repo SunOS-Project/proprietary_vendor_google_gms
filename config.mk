4b41ef28239dd5cc84435b0a8b40fe952f76a1b6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-09 00:04:26 +0530

    gms: Update to 12_202201
    Keep GoogleRestore at existing version because
    the new version seems to be crashing:
    
    E DatabaseUtils: java.lang.SecurityException: GoogleCertificatesRslt: Package signed with debug key (go/gsrlt)

--- config.mk
+++ config.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.gmsversion=12_202111 \
+    ro.com.google.gmsversion=12_202201 \
     ro.opa.eligible_device=true \
     ro.setupwizard.rotation_locked=true \
     setupwizard.theme=glif_v3_light
